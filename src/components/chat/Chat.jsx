910858fb8fae81584d9d6305443b7b9e681ed8ef
MinThetN <minthetn15@gmail.com>
2025-08-15 02:24:43 +0700

    update ui

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,29 @@
 import "./chat.css"
+// import { Phone } from 'lucide-react';
+// import { Video } from 'lucide-react';
+import { Info } from 'lucide-react';
 
 const chat = () => {
   return (
     <div className="chat">
-      <h1>chat</h1>
+      
+      <div className="top">
+        <div className="user">
+          <img src="./images/avatar.jpg" alt="" />
+          <div className="texts">
+            <span>User Name</span>
+            <p>my name is minthetnaung</p>
+          </div>
+        </div>
+        <div className="icons">
+          {/* <Phone />
+          <Video /> */}
+          <Info />
+        </div>
+      </div>
+
+      <div className="center"></div>
+      <div className="bottom"></div>
     </div>
   )
 }
--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,21 +1,106 @@
 import "./chatList.css"
 import { Search } from 'lucide-react';
-import { Plus } from 'lucide-react';
+import { Plus, Minus } from 'lucide-react';
+import { useState } from 'react';
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false)
   return (
     <div className="chatList">
       <div className="search">
-        
         <div className="searchBar">
           <Search />
           <input type="text" placeholder="Search Users" />
         </div>
-        <div className="add">
-          <Plus />
+        <div 
+          className={`add ${addMode ? 'active' : ''}`} 
+          onClick={() => setAddMode((prev => !prev))}
+        >
+          {addMode ? <Minus /> : <Plus />}
         </div>
-        
+      </div>
 
+      {/* Chat items container */}
+      <div className="chatItems">
+        <div className="item">
+          <img src="./images/avatar.jpg" alt="" />
+          <div className="texts">
+            <h4>User Name</h4>
+            <p>Hello</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="./images/avatar.jpg" alt="" />
+          <div className="texts">
+            <h4>User Name</h4>
+            <p>Hello</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="./images/avatar.jpg" alt="" />
+          <div className="texts">
+            <h4>User Name</h4>
+            <p>Hello</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="./images/avatar.jpg" alt="" />
+          <div className="texts">
+            <h4>User Name</h4>
+            <p>Hello</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="./images/avatar.jpg" alt="" />
+          <div className="texts">
+            <h4>User Name</h4>
+            <p>Hello</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="./images/avatar.jpg" alt="" />
+          <div className="texts">
+            <h4>User Name</h4>
+            <p>Hello</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="./images/avatar.jpg" alt="" />
+          <div className="texts">
+            <h4>User Name</h4>
+            <p>Hello</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="./images/avatar.jpg" alt="" />
+          <div className="texts">
+            <h4>User Name</h4>
+            <p>Hello</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="./images/avatar.jpg" alt="" />
+          <div className="texts">
+            <h4>User Name</h4>
+            <p>Hello</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <img src="./images/avatar.jpg" alt="" />
+          <div className="texts">
+            <h4>User Name</h4>
+            <p>Hello</p>
+          </div>
+        </div>
       </div>
     </div>
   )
