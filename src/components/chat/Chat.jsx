2ebc6c1edb672c3ad9907273866467a63b537e79
MinThetN <minthetn15@gmail.com>
2025-08-15 16:30:01 +0700

    update chat ui

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,8 +2,23 @@ import "./chat.css"
 // import { Phone } from 'lucide-react';
 // import { Video } from 'lucide-react';
 import { Info } from 'lucide-react';
+import { SmilePlus } from 'lucide-react';
+import { Camera } from 'lucide-react';
+import { Mic } from 'lucide-react';
+import { ImagePlus } from 'lucide-react';
+import EmojiPicker from "emoji-picker-react";
+import { useState } from "react";
 
-const chat = () => {
+
+const Chat = () => {
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleEmoji = e => {
+    setText((prev) => prev + e.emoji);
+    setShowEmojiPicker(false)
+  }
+
   return (
     <div className="chat">
       
@@ -18,14 +33,34 @@ const chat = () => {
         <div className="icons">
           {/* <Phone />
           <Video /> */}
-          <Info />
+          <Info size={20} />
         </div>
       </div>
 
       <div className="center"></div>
-      <div className="bottom"></div>
+
+      <div className="bottom">
+        <div className="icons">
+          <ImagePlus size={20} />
+          <Camera size={20} />
+          <Mic size={20} />
+        </div>
+        <input 
+        type="text" 
+        placeholder="Type a message" 
+        value={text}
+        onChange={(e) => setText(e.target.value)} />
+        <div className="emoji" >
+          <SmilePlus size={20} onClick={() => setShowEmojiPicker ((prev) => !prev)}/>
+          <div className="picker">
+            <EmojiPicker open={showEmojiPicker} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+
     </div>
   )
 }
 
-export default chat
+export default Chat
